2073f833b1738e0cbf5b5aa78b5e618b9609285d
Mars-2dot <ilya.boldyrev36@gmail.com>
2021-04-26 14:57:41 +0300

    fix cmake

--- server/cmake/ExretnalProjectspdlog.cmake
+++ server/cmake/ExretnalProjectspdlog.cmake
@@ -8,7 +8,7 @@ set(LOG_PATH ${CMAKE_BINARY_DIR}/3rd/external_files/log)
 set(SPDLOG_URL https://github.com/gabime/spdlog.git)
 
 set(SPDLOG_ROOT ${CMAKE_BINARY_DIR}/3rd/spdlog)
-set(SPDLOG_LIB_PATH ${SPDLOG_ROOT}/Debug/spdlogd.lib)
+set(SPDLOG_LIB_PATH ${SPDLOG_ROOT}/spdlogd.lib)
 set(SPDLOG_INCLUDE_DIRS ${SPDLOG_ROOT}/include)
 set(SPDLOG_INSTALL_CMAKE ${CMAKE_BINARY_DIR}/dependencies/spdlog)
 
@@ -17,12 +17,13 @@ ExternalProject_Add(spdlog
     DOWNLOAD_DIR      ${SPDLOG_ROOT}
     SOURCE_DIR        ${SPDLOG_ROOT}
     BINARY_DIR        ${SPDLOG_ROOT}
+    INSTALL_DIR       ${SPDLOG_INSTALL_CMAKE}
     TMP_DIR           ${TMP_PATH}
     STAMP_DIR         ${STAMP_PATH}
     LOG_DIR 	      ${LOG_PATH}
     GIT_REPOSITORY    ${SPDLOG_URL}
     GIT_TAG           v1.8.5
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${SPDLOG_INSTALL_CMAKE}
+    INSTALL_COMMAND ""
     BUILD_COMMAND     ${CMAKE_COMMAND} --build ${SPDLOG_ROOT} --config Release
     BUILD_BYPRODUCTS  ${SPDLOG_LIB_PATH}
 )
@@ -47,3 +48,29 @@ set_target_properties(spdlog::spdlog PROPERTIES
 )
 add_dependencies(spdlog::spdlog spdlog)
 
+add_custom_command(
+        TARGET spdlog POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy_directory
+                ${SPDLOG_INCLUDE_DIRS}
+                ${SPDLOG_INSTALL_CMAKE}/include)
+add_custom_command(
+        TARGET spdlog POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_BINARY_DIR}/3rd/spdlog/spdlogd.lib
+                ${SPDLOG_INSTALL_CMAKE}/lib/spdlogd.lib)
+add_custom_command(
+        TARGET spdlog POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy_directory
+                ${CMAKE_BINARY_DIR}/3rd/spdlog/CMakeFiles/Export/lib/
+                ${SPDLOG_INSTALL_CMAKE}/lib)
+add_custom_command(
+        TARGET spdlog POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_BINARY_DIR}/3rd/spdlog/spdlogConfig.cmake
+                ${CMAKE_BINARY_DIR}/3rd/spdlog/spdlogConfigVersion.cmake
+                ${CMAKE_BINARY_DIR}/dependencies/spdlog/lib/cmake/spdlog)
+add_custom_command(
+        TARGET spdlog POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_BINARY_DIR}/3rd/spdlog/spdlog.pc
+                ${SPDLOG_INSTALL_CMAKE}/lib/pkgconfig)
